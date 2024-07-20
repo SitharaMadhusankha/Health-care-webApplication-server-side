610d8c9f7aefd44183d2b0024b773b84d7782699
SitharaMadhusankha <166124345+SitharaMadhusankha@users.noreply.github.com>
2024-07-20 20:36:45 +0530

    create Doctor and Patient class and constructors

---
+++ Health Care/src/Doctor.java
@@ -0,0 +1,16 @@
+public class Doctor {
+
+    int doctorId;
+    String name;
+    String specialization;
+    String contactNumber;
+
+    public Doctor(int doctorId, String name, String specialization, String contactNumber) {
+        this.doctorId = doctorId;
+        this.name = name;
+        this.specialization = specialization;
+        this.contactNumber = contactNumber;
+    }
+
+
+}
---
+++ Health Care/src/Patient.java
@@ -0,0 +1,14 @@
+public class Patient {
+
+    String name;
+    String birthday;
+    String contactNumber;
+    String patientId;
+
+    public Patient(String name, String birthday, String contactNumber, String patientId) {
+        this.name = name;
+        this.birthday = birthday;
+        this.contactNumber = contactNumber;
+        this.patientId = patientId;
+    }
+}
